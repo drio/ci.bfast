25221e42472845f88c475fffa2878e76ada8c734
David Rio Deiros <driodeiros@gmail.com>
2011-05-16 07:35:27 -0500

    * Detecting if it is necessary to perform compilations

--- ci.bfast.sh
+++ ci.bfast.sh
@@ -20,14 +20,33 @@ branches=(bfast bfast-bwa)
 tmp="/tmp/$0.$(echo $RANDOM)"
 git_url="git://bfast.git.sourceforge.net/gitroot/bfast/"
 
+have_changes()
+{
+  [ `git pull | wc -l` == 1 ] && return 0 || return 1
+}
+
+before_darwin()
+{
+  if [ -d $brand ];then
+    cd $brand
+    have_changes
+    [ $? -eq 1 ] && return 1 || return 0
+  else
+    git clone $git_url/$branch
+  fi
+}
+
 # 
 # Main
 for box in "${boxes[@]}";do
   for branch in "${branches[@]}"; do
     if [ $box == "darwin" ];then
-      ci_darwin $branch
+      before_darwin
+      after_darwin
     else
       echo "$box:$branch"
+      before_vb
+      after_vb 
     fi
   done
 done
