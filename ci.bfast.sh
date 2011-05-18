cf6ef44626fb76bc28986a32ed4b0c2264a094c6
David Rio Deiros <driodeiros@gmail.com>
2011-05-18 07:21:44 -0500

    * We build the tool now.

--- ci.bfast.sh
+++ ci.bfast.sh
@@ -6,6 +6,8 @@ boxes=(lucid32 lucid64)
 branches=(bfast bfast-bwa)
 tmp="/tmp/$0.$(echo $RANDOM)"
 git_url="git://bfast.git.sourceforge.net/gitroot/bfast"
+logs_dir="`pwd`/logs"
+ts=`date +%d.%m.%y.%H.%S.%s`
 
 log()
 {
@@ -16,21 +18,42 @@ look_for_changes()
 {
   local b=$1
   if [ -d $b ];then
+    log "pulling"
     cd $b
     [ `git pull | wc -l` == 1 ] && echo 0 || echo 1
+    cd ..
   else
+    log "cloning"
     git clone -q $git_url/$b
     echo 1 
   fi
 }
 
+compile_pipe()
+{
+  local b=$1
+  local arch=$2
+  cd $b 
+  log "Building $b branch in arch $arch"
+  (
+  sh ./autogen.sh && ./configure && make && make check
+  ) &> $logs_dir/$arch.$b.$ts.log
+  log "Exit status: $?"
+  cd ..
+}
+
 # 
 # Main
+# The local machine is a Darwin(OSX) box. 
+#
+mkdir -p $logs_dir
 for branch in "${branches[@]}"; do
+  log "Looking for changes in branch: $branch"
   changes=$(look_for_changes $branch)
   if [ $changes == "1" ];then
-    log "$branch: CHANGES"
+    log "$branch: NEW commits."
+    compile_pipe $branch "Darwin"
   else
-    log "$branch: NO changes"
+    log "$branch: NO new commits"
   fi
 done
