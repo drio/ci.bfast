b747d1e667baa51197085519fadf6cf8c6be0db6
David Rio Deiros <driodeiros@gmail.com>
2011-05-19 07:20:41 -0500

    * Minimal changes in the main script * Looking into vagrant's code to figure out how to skip the ssh   password so we can run commands in the virtualbox from the host.

--- ci.bfast.sh
+++ ci.bfast.sh
@@ -18,8 +18,9 @@ look_for_changes()
 {
   local b=$1
   if [ -d $b ];then
-    log "pulling"
     cd $b
+    log "make clean" ; make clean &>/dev/null
+    log "pulling"
     [ `git pull | wc -l` == 1 ] && echo 0 || echo 1
     cd ..
   else
@@ -47,13 +48,23 @@ compile_pipe()
 # The local machine is a Darwin(OSX) box. 
 #
 mkdir -p $logs_dir
+active_branches=""
 for branch in "${branches[@]}"; do
   log "Looking for changes in branch: $branch"
   changes=$(look_for_changes $branch)
   if [ $changes == "1" ];then
     log "$branch: NEW commits."
+    active_branches="$active_branches $branch"  
     compile_pipe $branch "Darwin"
   else
     log "$branch: NO new commits"
+    active_branches="$active_branches $branch"  
   fi
+done
+
+# If new commits in branch, run pipe in the 
+# different virtual boxes
+for branch in $active_branches
+do
+  echo $branch
 done
